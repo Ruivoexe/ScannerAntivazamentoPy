fc504f2cc59c921c36174c6cc4cc96ce1079f701
Vinicius Ferreira <41nicius.aleg@gmail.com>
2026-04-02 04:01:52 -0300

    010 - cpf refinado funcional

--- app/main.py
+++ app/main.py
@@ -32,11 +32,17 @@ def scan(
     table = Table(title="Resumo do Scan")
     table.add_column("Métrica", style="cyan")
     table.add_column("Valor", style="magenta")
+
     table.add_row("Escaneados", str(sumario_main["arq_total_scan"]))
     table.add_row("Capturas",str(sumario_main["total_capturas"]))
+    table.add_row("CPF válidos", str(sumario_main["cpfs_validos"]))
+    table.add_row("CPF inválidos", str(sumario_main["cpfs_invalidos"]))
 
     for tipo,count in sumario_main["tipo_captura"].items():
         table.add_row(f"Tipo:{tipo}",str(count))
+
+    for severidade, count in sumario_main["severidade_captura"].items():
+        table.add_row(f"Severidade: {severidade}", str(count))
 
     console.print(table)
     console.print(f"\n[green]Relatório salvo em:[/green] {output_path}")
